0e014a9bee424e17df0836a3ce843ae3849725de
Sebastian Dow <62687662+SebastianDow@users.noreply.github.com>
2025-10-07 10:03:48 -0500

    Create conditional_statements_in_c.c

---
+++ C/conditional_statements_in_c.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int * get_num();
+
+int main(){
+    unsigned int * value = malloc( sizeof( unsigned int ) );
+    value = get_num();
+    switch( * value ) {
+        case 1:
+            printf( "one" );
+            break;
+        case 2:
+            printf( "two" );
+            break;
+        case 3:
+            printf( "three" );
+            break;
+        case 4:
+            printf( "four" );
+            break;
+        case 5:
+            printf( "five" );
+            break;
+        case 6:
+            printf( "six" );
+            break;
+        case 7:
+            printf( "seven" );
+            break;
+        case 8:
+            printf( "eight" );
+            break;
+        case 9:
+            printf( "nine" );
+            break;
+        default:
+            printf( "Greater than 9" );
+    }
+    return 0;
+}
+
+unsigned int * get_num(){
+    unsigned int * value = malloc( sizeof( unsigned int ) );
+    scanf( "%d", value );
+    return value;
+}
